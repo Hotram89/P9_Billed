3f63c273502c29238aa8f581f33bcd1468bab668
Hotram89 <marion.dournel@gmail.com>
2022-06-18 15:36:43 +0200

    WIP test NewBills.js

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -5,11 +5,12 @@
 import { fireEvent, screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
-import { ROUTES_PATH } from "../constants/routes.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import { formatDate } from "../app/format.js";
 import Bills from "../containers/Bills.js";
 import router from "../app/Router.js";
+import NewBillUI from "../views/NewBillUI.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -42,6 +43,7 @@ describe("Given I am connected as an employee", () => {
      *
      */
     test("Then bills should be ordered from earliest to latest", () => {
+       // const bills =(new Bills()).getBills()
       document.body.innerHTML = BillsUI({ data: bills });
       const dates = screen.getAllByLabelText("Date").map((a) => a.innerHTML);
       // const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
@@ -70,56 +72,66 @@ describe("Given I am connected as an employee", () => {
      */
 
     test("When I click on eye-icon, modal opens", () => {
-      // DOM elements
+      // Que la construction HTML
       document.body.innerHTML = BillsUI({ data: bills });
+
+      // mock store
+      const store = null;
+    // comportement
+
+   const billsList = new Bills({
+         document,
+         onNavigate,
+         store,
+         localStorage: window.localStorage,
+       });
+
       //mock modal
       $.fn.modal = jest.fn();
-      // mock store
-      const mockStore = null;
-
       const eye = screen.getAllByTestId("icon-eye")[0];
-      const bills = new Bills({
-        document,
-        onNavigate,
-        mockStore,
-        localStorage: window.localStorage,
-      });
+      const handleClickIconEye = jest.fn(() =>
+        billsList.handleClickIconEye(eye)
+      );
 
-      const handleClickIconEye = jest.fn(() => bills.handleClickIconEye(eye));
       eye.addEventListener("click", handleClickIconEye);
       fireEvent.click(eye);
-      const modale = document.getElementById('modaleFile')
-
       expect(handleClickIconEye).toHaveBeenCalled();
-     expect(modale.classList.contains("show")).toBeTruthy()
-
-      expect(modale).toBeTruthy();
+      
     });
 
     /**
      * Open "nouvelle note de frais"
      * 
      */
-    // test("When I click on Nouvelle note de frais button, go to NewBill", () => {
-    //   // DOM elements
-    //   document.body.innerHTML = BillsUI({ data: bills });
-      
-    //   const billss = new Bills({
-    //     document,
-    //     onNavigate,
-    //     store: null,
-    //     localStorage: window.localStorage,
-    //   });
-    //   const newBillBtn = screen.getByTestId("btn-new-bill");
-    //   //mock handleClickNewBillMock
-    //   const handleClickNewBillMock = jest.fn(billss.handleClickNewBill)
-    //   newBillBtn.addEventListener('click', handleClickNewBillMock)
-    //   fireEvent.click(newBillBtn)
-    //   expect(handleClickNewBillMock).toHaveBeenCalled();
-    //   const noteDeFrais = screen.getAllByText("Envoyer une note de frais")
-    //   expect(noteDeFrais).toBeTruthy()
+     test("When I click on Nouvelle note de frais button, go to NewBill", () => {
+       // DOM elements
 
-    
-    // });
+       const onNavigate = (pathname) => {
+         document.body.innerHTML = ROUTES({ pathname });
+       };
+
+       const store = null
+
+       const billsMock = new Bills({
+         document,
+         onNavigate,
+         store,
+         localStorage: window.localStorage,
+       });
+
+       document.body.innerHTML = BillsUI({ data: bills });
+       //   //mock handleClickNewBillMock
+       const buttonNewBill = screen.getByTestId("btn-new-bill");
+
+       const handleClickNewBill = jest.fn((e) =>
+         billsMock.handleClickNewBill(e)
+       );
+
+       buttonNewBill.addEventListener("click", handleClickNewBill);
+       fireEvent.click(buttonNewBill);
+       expect(handleClickNewBill).toHaveBeenCalled();
+       const noteDeFrais = screen.getByText("Envoyer une note de frais");
+       expect(noteDeFrais).toBeTruthy();
+     });
   });
 });
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -9,10 +9,26 @@ import NewBill from "../containers/NewBill.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then I give a good format for proof file", () => {
+
+        const onNavigate = (pathname) => {
+            document.body.innerHTML = ROUTES({ pathname });
+          };
       const html = NewBillUI()
       document.body.innerHTML = html
       //to-do write assertion
+     
+      const mockStore = null
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage: window.localStorage,
+      });
+
+      const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+
+
     })
   })
 })
