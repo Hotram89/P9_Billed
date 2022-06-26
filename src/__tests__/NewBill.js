53f371fdb167c698392c4a77e41e65cf7a8461ea
Hotram89 <marion.dournel@gmail.com>
2022-06-26 13:20:35 +0200

    WIP BIlls

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -37,7 +37,7 @@ describe("Given I am connected as an employee", () => {
         
     })
 
-    test("The input for proof file change", () => {
+    test("The input for proof file change with a correct image", () => {
     
       //charge le html d'une page NewBill
       document.body.innerHTML = NewBillUI();
@@ -72,5 +72,112 @@ describe("Given I am connected as an employee", () => {
 
       expect(handleChangeFileMock).toHaveBeenCalled();
     });
+
+    test("The input for proof file change with a wrong image", () => {
+    
+        //charge le html d'une page NewBill
+        document.body.innerHTML = NewBillUI();
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname });
+        };
+  
+        //   //propose ces comportements pour la construction de la page
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          store: store,
+          localStorage: window.localStorage,
+        });
+        //mock la fonction appelée
+        const handleChangeFileMock = jest.fn(newBill.handleChangeFile);
+        //recupere l'input du fichier a ajouter
+        const inputFileButton = screen.getByTestId("file");
+        inputFileButton.addEventListener("change", handleChangeFileMock);
+  
+        //   mock un fichier
+        const file = new File(["myProof.doc"], "myProof.doc", {
+          type: "image/jpg",
+        });
+  
+        fireEvent.change(inputFileButton, {
+          target: {
+            files: [file],
+          },
+        });
+  
+        expect(handleChangeFileMock).toHaveBeenCalled();
+      });
   })
 })
+
+// test d'intégration GET
+describe("Given I am a user connected as Employee", () => {
+    describe("When I navigate to NewBills", () => {
+      test("fetches bills from mock API POST", async () => {
+        localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.append(root)
+        router()
+        window.onNavigate(ROUTES_PATH.NewBill)
+
+        
+        const form = screen.getByTestId("from-new-bill")
+
+        fireEvent.submit(form)
+        const contentPending  = await screen.getByText("En attente (1)")
+        expect(contentPending).toBeTruthy()
+        const contentRefused  = await screen.getByText("Refusé (2)")
+        expect(contentRefused).toBeTruthy()
+        expect(screen.getByTestId("big-billed-icon")).toBeTruthy()
+      })
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills")
+        Object.defineProperty(
+            window,
+            'localStorage',
+            { value: localStorageMock }
+        )
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee',
+          email: "a@a"
+        }))
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.appendChild(root)
+        router()
+      })
+      test("fetches bills from an API and fails with 404 message error", async () => {
+  
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list : () =>  {
+              return Promise.reject(new Error("Erreur 404"))
+            }
+          }})
+        window.onNavigate(ROUTES_PATH.Dashboard)
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/)
+        expect(message).toBeTruthy()
+      })
+  
+      test("fetches messages from an API and fails with 500 message error", async () => {
+  
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list : () =>  {
+              return Promise.reject(new Error("Erreur 500"))
+            }
+          }})
+  
+        window.onNavigate(ROUTES_PATH.Dashboard)
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/)
+        expect(message).toBeTruthy()
+      })
+    })
+  
+    })
+  })
+  
